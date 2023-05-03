26cda00cdfa05abb5ea321a97e57ceb3f13df03c
Janidu Lankage <janidulnkg1@gmail.com>
2023-05-03 10:19:07 +0530

    commit User Management CRUD_ASP.NET-REST-API

---
+++ User-Management/User-Management/Controllers/UserController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using User_Management.Models;
+
+namespace User_Management.Controllers
+{
+
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private static List<UserDto> listUsers = new List<UserDto>()
+        {
+            new UserDto()
+            {
+                firstName = "Janidu",
+                lastName  = "Lankage",
+                email = "janidulnkg1@gmail.com",
+                phone = "0762200954",
+                address = "Colombo, Sri Lanka"
+            },
+            new UserDto()
+            {
+                firstName = "Dinushika",
+                lastName  = "Samarasingha",
+                email = "sdinushika99@gmail.com",
+                phone = "0742146066",
+                address = "Colombo, Sri Lanka"
+            }
+
+
+        };
+
+        [HttpGet("/users")]
+        public IActionResult GetUsers()
+        {
+            if(listUsers.Count > 0)
+            {
+                return Ok(listUsers);
+            }
+            return NoContent();
+        }
+
+        [HttpGet("/user/get/{id}")]
+        public IActionResult GetUser(int id)
+        {
+            if (id >= 0 && id < listUsers.Count)
+            {
+                return Ok(listUsers[id]);
+            }
+            return NotFound();
+        }
+
+        [HttpPost("/user/create")]
+        public IActionResult AddUser(UserDto user)
+        {
+            if( user.email.Equals("user@example.com"))
+            {
+                ModelState.AddModelError("Email", "This Email is not authorized");
+                return BadRequest(ModelState);
+            }
+
+            listUsers.Add(user);
+            return Ok();
+        }
+
+        [HttpPut("/user/update/{id}")]
+        public IActionResult UpdateUser(int id, UserDto user)
+        {
+            if (user.email.Equals("user@example.com"))
+            {
+                ModelState.AddModelError("Email", "This Email is not authorized");
+                return BadRequest(ModelState);
+            }
+
+            if (id >= 0 && id <= listUsers.Count)
+            {
+                listUsers[id] = user;
+            }
+            return Ok();
+        }
+
+        [HttpDelete("/user/delete/{id}")]
+        public IActionResult DeleteUser(int id) { 
+            if(id >= 0 && id < listUsers.Count)
+            {
+                listUsers.RemoveAt(id);
+            }
+
+            return NoContent();
+        }
+    }
+}
---
+++ User-Management/User-Management/Models/UserDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace User_Management.Models;
+
+public class UserDto
+{
+    [Required]
+    public string firstName { get; set; } = "";
+
+    [Required(ErrorMessage = "Please Provide your Last Name")]
+    public string lastName { get; set; } = "";
+    [Required, EmailAddress]
+    public string email { get; set; } = "";
+    public string phone { get; set; } = "";
+    [Required]
+    [MinLength(5, ErrorMessage = "The Address should be atleast 5 characters")]
+    [MaxLength(1000, ErrorMessage = "The Address should be less than 1000 characters")]
+    public string address { get; set; } = "";
+}
