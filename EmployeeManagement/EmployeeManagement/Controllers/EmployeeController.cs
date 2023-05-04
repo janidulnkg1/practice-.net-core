8edf4c9852a1af6babc7d21e1dd1136a647d76f4
Janidu Lankage <janidulnkg1@gmail.com>
2023-05-04 10:25:01 +0530

    commit employeeController

--- EmployeeManagement/EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/EmployeeManagement/Controllers/EmployeeController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System.Data;
+using EmployeeManagement.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace EmployeeManagement.Controllers
 {
@@ -7,5 +11,63 @@ namespace EmployeeManagement.Controllers
     [ApiController]
     public class EmployeeController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public EmployeeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("/employees/getall")]
+        public JsonResult Get(){
+
+            string query = @"select * from employee";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+            MySqlDataReader myReader;
+            using (MySqlConnection myCon = new MySqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+                return new JsonResult(table);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
