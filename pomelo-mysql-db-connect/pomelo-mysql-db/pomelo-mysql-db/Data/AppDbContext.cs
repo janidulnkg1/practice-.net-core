710b122f0ebd23cf9326f48c8476af8716fcee60
janidulnkg1 <janidulnkg1@gmail.com>
2023-11-14 16:31:08 +0530

    Add pomelo-mysql-db-connect
    Connecting to MYSQl, EFCore / Identity  migrations using Pomelo

---
+++ pomelo-mysql-db-connect/pomelo-mysql-db/pomelo-mysql-db/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pomelo_mysql_db.Model;
+
+namespace pomelo_mysql_db.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionstring = "server=localhost;port=3306;database=testdb;user=janidu;password=Janidu@Q_Excess";
+
+            var serverVersion = new MySqlServerVersion(new Version(5, 7, 42));
+            optionsBuilder.UseMySql(connectionstring, serverVersion);
+        }
+
+    }
+}
---
+++ pomelo-mysql-db-connect/pomelo-mysql-db/pomelo-mysql-db/Model/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace pomelo_mysql_db.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Title { get; set; }
+    }
+}
